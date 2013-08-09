964a7cd5ed11d32e249198daec804bba646a33b8
Matthew Lu <matthew.lu@permissioncorp.com>
2013-08-09 17:19:53 +1000

    second commit

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Matthew Lu on 9/08/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
